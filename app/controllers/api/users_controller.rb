d1f4cfbf27213215d5b9f9003e367ccc1e7284d7
addriv <adrivero89@gmail.com>
2017-10-01 18:46:46 -0700

    Update UsersController to generate new team when creating new user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,11 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # REMOVE ALL THIS AFTER IMPLEMENTING FORCED TEAM MODAL
+      @team = Team.create(name: 'New Team', lead_id: @user.id)
+      UserTeam.create(member_id: @user.id, team_id: @team.id)
+      # REMOVE ALL THIS AFTER IMPLEMENTING FORCED TEAM MODAL
+
       login(@user)
       render :show
     else
