7dc40bedd12081e3ec97d4d47b058cb07c7fd09b
addriv <adrivero89@gmail.com>
2017-10-31 15:52:00 -0700

    Update users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,6 +5,19 @@ class Api::UsersController < ApplicationController
       # REMOVE ALL THIS AFTER IMPLEMENTING FORCED TEAM MODAL
       @team = Team.create(name: 'New Team', lead_id: @user.id)
       UserTeam.create(member_id: @user.id, team_id: @team.id)
+      UserTeam.create(member_id: 2, team_id: @team.id)
+      UserTeam.create(member_id: 3, team_id: @team.id)
+      UserTeam.create(member_id: 4, team_id: @team.id)
+      UserTeam.create(member_id: 5, team_id: @team.id)
+      @project = Project.create(name: 'New Project', lead_id: @user.id, team_id: @team.id)
+      Task.create(title: 'This is the tasks list', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'Click Add Task to Add More', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'New Tasks Go Here', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'New Tasks Go Here', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: '<---- Click Check to Mark Complete', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'Click each task to view details', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'You can assign other users to each task', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
+      Task.create(title: 'Add a Task Description in the Details', creator_id: @user.id, assignee_id: @user.id, project_id: @project.id)
       # REMOVE ALL THIS AFTER IMPLEMENTING FORCED TEAM MODAL
 
       login(@user)
@@ -18,5 +31,9 @@ class Api::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :name, :password)
+  end
+
+  def create_demo_team
+
   end
 end
